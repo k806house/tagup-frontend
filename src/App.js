b0ca4e9e3e683c1978b3e070643f35f8bdaf12df
19113494 <lizabezlutskaya@gmail.com>
2021-11-13 16:40:43 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Input, Card, Layout } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout id="layout">
+        <Header className="App-header">
+          <div style={{padding: '0 16px'}}>
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <img src="//static.wbstatic.net/i/header/logo-v1.svg" alt="Wildberries"/>
+            <Input allwClear defaultValue="Search" id="inputSearch"/>
+            {/* <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p> */}
+          </div>
+        </Header>
+        <Content>
+          <div style={{padding: '0 50px'}}>
+            <Card bordered={false}>
+              <p>Card content</p>
+            </Card>
+          </div>
+        </Content>
+      </Layout>
+      
+      {/* <header className="App-header">
+        <div style={{padding: '0 16px'}}> */}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <img src="//static.wbstatic.net/i/header/logo-v1.svg" alt="Wildberries"/>
+          <Input allwClear defaultValue="Search" id="inputSearch"/>
+        </div>
+      </header> */}
+      {/* <div style={{padding: '0 16px'}}>
+        <Card bordered={false}>
+          <p>Card content</p>
+        </Card>
+      </div> */}
+
     </div>
   );
 }
